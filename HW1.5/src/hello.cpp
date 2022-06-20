5321afc511689041f05a436efa956d4f4a43c125
kaixinguor <linr.w@163.com>
2022-06-21 07:37:55 +0800

    update

--- HW1.5/src/hello.cpp
+++ HW1.5/src/hello.cpp
@@ -1,8 +1,14 @@
 #include "hello.h"
 #include <iostream>
 #include "glog/logging.h"
+#include "gflags/gflags.h"
+
+DECLARE_int32(print_times);
 
 void sayHello() {
     //std::cout<<"Hello SLAM"<<std::endl;
-    LOG(INFO)<<"Hello SLAM";
+
+    for(int i=0; i<FLAGS_print_times;i++) {
+        LOG(INFO)<<"Hello SLAM";
+    }
 }
--- HW1.5/useHello.cpp
+++ HW1.5/useHello.cpp
@@ -1,7 +1,12 @@
 #include "hello.h"
 #include "glog/logging.h"
+#include "gflags/gflags.h"
+
+DEFINE_int32(print_times, 1, "print times");
 
 int main( int argc, char** argv ) {
+
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
 
     google::InitGoogleLogging(argv[0]);
     FLAGS_log_dir = "./";
